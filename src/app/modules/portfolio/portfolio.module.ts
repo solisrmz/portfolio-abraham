d1c79a93b9aaa452f1acc1604a6b622a973ebef2
Abraham <jsolis@ssaver.gob.mx>
2022-03-16 20:53:44 -0600

    ADD - Directorio para los componentes

--- src/app/modules/portfolio/portfolio.module.ts
+++ src/app/modules/portfolio/portfolio.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { PortfolioRoutingModule } from './portfolio-routing.module';
 import { HomePortfolioComponent } from './home-portfolio.component';
 import { MaterialModule } from '../../material.module';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    HomePortfolioComponent
+    HomePortfolioComponent,
+    AboutMeComponent
   ],
   imports: [
     CommonModule,
