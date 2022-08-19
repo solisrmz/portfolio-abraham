dd2f58c8c99940830bd9f4863c08b892462c0904
Abraham <imsolis@hotmail.com>
2022-08-19 09:42:08 -0500

    ADD - Nueva seccion de presentacion en ambos idiomas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ export function HttpLoaderFactory(http: HttpClient) {
         deps: [HttpClient]
       },
       defaultLanguage: 'en',
-    })
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
